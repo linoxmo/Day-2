541ad301b1765537379df37a83efe7a8af333d9d
tmagoudi <tmagoudi@f1br6s7.paris.42.school>
2026-06-10 19:53:18 +0200

    clean du code

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -69,56 +69,6 @@ class Plant:
         self.grow()
 
 
-class Flower(Plant):
-    def __init__(self, name: str, height: float, age: int,
-                 growth: float, color: str) -> None:
-        super().__init__(name, height, age, growth)
-        self._color = color
-        self._bloomed = False
-
-    def bloom(self) -> None:
-        if not (self._bloomed):
-            print("[asking the rose to bloom]")
-            self._bloomed = True
-            self.show()
-
-    def show(self) -> None:
-        super().show()
-        print(f"Color: {self._color}")
-        if not (self._bloomed):
-            print(f"{self.name} has not bloomed yet!")
-        if (self._bloomed):
-            print(f"{self.name} is blooming beautifully!")
-
-
-class Tree(Plant):
-    def __init__(self, name: str, height: float, age: int,
-                 growth: float, trunk_diameter: float) -> None:
-        super().__init__(name, height, age, growth)
-        self._trunk_diameter = trunk_diameter
-
-    def produce_shade(self) -> None:
-        print("[asking the oak to produce shade]")
-        print(f"Tree {self.name} now produces a shade", end="")
-        print(f"of {self.height}cm long and {self._trunk_diameter} cm wide.")
-
-    def show(self) -> None:
-        super().show()
-        print(f"Trunk diameter: {self._trunk_diameter}")
-
-
-class Vegetable(Plant):
-    def __init__(self, name: str, height: float, age: int,
-                 growth: float, season: str, NV: float) -> None:
-        super().__init__(name, height, age, growth)
-        self._season = season
-        self._NV = NV
-
-    def show(self) -> None:
-        super().show()
-        print(f"Harvest season: {self._season}\nNutritional value: {self._NV}")
-
-
 print("=== Garden Security System ===")
 
 rose = Plant("Rose", 15.0, 10, 1.0)
